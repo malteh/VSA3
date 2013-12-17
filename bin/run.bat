887fc383d28d8323ff7e61c93f0d17caa6700c40
Darius <dariusmurawski@hotmail.com>
2013-12-17 18:04:41 +0100

    Einbauen und so

---
+++ bin/run.bat
@@ -0,0 +1,3 @@
+start java -cp .;bank.jar bank.Bank D-PC.fritz.box 22334 NDEA -v
+start java -cp .;filiale.jar filiale.Filiale D-PC.fritz.box 22334 NDEA  1 -v
+start java -cp .;geldautomat.jar geldautomat.Geldautomat D-PC.fritz.box 22334 NDEA  1 -v
